0bd0a552b5d60e04a3923d95bc2e5725879db86b
Minor Gordon <minorg@server.fake>
2022-09-21 16:20:04 -0400

    change output_format

--- dev.sh
+++ dev.sh
@@ -12,6 +12,6 @@ COLLECTION=Thinker-Doer
 #OUTPUT_DATA=Bildungsroman.ttl
 #OUTPUT_FORMAT=ttl-rdf
 OUTPUT_DATA=Bildungsroman-gui
-OUTPUT_FORMAT=bootstrap-exhibition
+OUTPUT_FORMAT=exhibition
 
 python3 gui-action/action.py --debug 1 --dev --base-url-path /$COLLECTION --configuration-file-path Bildungsroman/configuration.json --id Bildungsroman --input-data Bildungsroman/$COLLECTION --input-format markdown --output-data $OUTPUT_DATA --output-format $OUTPUT_FORMAT
